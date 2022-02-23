bc4bf19b815fc63a51e902adc70d2d464cdcd014
cd-Roid <giyawe98@gmail.com>
2022-02-23 11:50:27 +0100

    used reducer to remove from cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.less";
-import { Card } from "antd";
+import { Card, Button } from "antd";
 import Meta from "antd/lib/card/Meta";
 import data from "./mockData/data";
 
 function App() {
 	const [products, setProducts] = useState(useSelector((state) => state.test));
+	const [cart, setCart] = useState(useSelector((state) => state.cart));
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -24,17 +25,34 @@ function App() {
 			dispatch({ type: "ADD_TO_CART", payload: newState });
 		}
 	};
+	const removeFromCart = (item) => {
+		if (cart.length === 0) {
+			console.log("Cart is Empty");
+			return;
+		} else {
+			if (cart.includes(item)) {
+				const newState = cart.filter((el) => el.title !== item.title);
+				setCart(newState);
+				dispatch({ type: "REMOVE_FROM_CART", payload: newState });
+			}
+		}
+	};
+
 	return (
 		<div className="App">
 			{products
 				? products.map((item) => (
 						<div>
 							<Card
+								onClick={() => addToCart(item)}
 								style={{ width: 240 }}
 								cover={<img alt={item.title} src={item.image} />}
 							>
 								<Meta title={item.title} description={item.description} />
 							</Card>
+							<Button type="primary" onClick={() => removeFromCart(item)}>
+								Subtract
+							</Button>
 						</div>
 				  ))
 				: "No Products available"}
