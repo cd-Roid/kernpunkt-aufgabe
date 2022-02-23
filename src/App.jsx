32abade1e1d00bc19c443604deaea499c990e9bc
cd-Roid <giyawe98@gmail.com>
2022-02-23 14:49:57 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.less";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import DetailProductPage from "./pages/DetailProductPage";
 
 function App() {
 	return (
 		<div className="App">
-			<HomePage />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/:title" element={<DetailProductPage />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
